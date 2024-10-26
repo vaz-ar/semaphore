89a2ddf6b51695abb614d4d52a31955bcbf8240d
Denis Gukov <fiftin@outlook.com>
2024-10-26 12:56:17 +0000

    feat(be): ansible-semaphore -> semaphoreui

--- db_lib/TerraformApp.go
+++ db_lib/TerraformApp.go
@@ -8,9 +8,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/ansible-semaphore/semaphore/db"
-	"github.com/ansible-semaphore/semaphore/pkg/task_logger"
-	"github.com/ansible-semaphore/semaphore/util"
+	"github.com/semaphoreui/semaphore/db"
+	"github.com/semaphoreui/semaphore/pkg/task_logger"
+	"github.com/semaphoreui/semaphore/util"
 )
 
 type TerraformApp struct {
