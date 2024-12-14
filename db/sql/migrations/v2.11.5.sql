645668dc76f889c038bba651cd6c26517ae68bef
Denis Gukov <denguk@gmail.com>
2024-12-15 00:49:02 +0500

    feat(db): add migration for tf

---
+++ db/sql/migrations/v2.11.5.sql
@@ -0,0 +1,17 @@
+create table project__terraform_inventory_alias(
+  `alias` varchar(100) primary key,
+  `project_id` int,
+  `inventory_id` int,
+  `auth_key_id` int,
+  foreign key (`project_id`) references project(`id`) on delete cascade,
+  foreign key (`inventory_id`) references project__inventory(`id`) on delete cascade,
+  foreign key (`auth_key_id`) references access_key(`id`)
+);
+
+create table project__terraform_inventory_state(
+  `id` integer primary key autoincrement,
+  `inventory_id` int,
+  `state` text,
+  `created` datetime NOT NULL,
+  foreign key (`inventory_id`) references project__inventory(`id`) on delete cascade
+);
