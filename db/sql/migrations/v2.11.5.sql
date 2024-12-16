58c48c27e6ac2cbd883df38fc9b76cc263f2a80a
Denis Gukov <denguk@gmail.com>
2024-12-16 05:00:44 +0500

    feat(tf): add task id to state

--- db/sql/migrations/v2.11.5.sql
+++ db/sql/migrations/v2.11.5.sql
@@ -1,8 +1,8 @@
 create table project__terraform_inventory_alias(
   `alias` varchar(100) primary key,
-  `project_id` int,
-  `inventory_id` int,
-  `auth_key_id` int,
+  `project_id` int NOT NULL,
+  `inventory_id` int NOT NULL,
+  `auth_key_id` int NOT NULL,
   foreign key (`project_id`) references project(`id`) on delete cascade,
   foreign key (`inventory_id`) references project__inventory(`id`) on delete cascade,
   foreign key (`auth_key_id`) references access_key(`id`)
@@ -10,10 +10,12 @@ create table project__terraform_inventory_alias(
 
 create table project__terraform_inventory_state(
   `id` integer primary key autoincrement,
-  `project_id` int,
-  `inventory_id` int,
-  `state` text,
+  `project_id` int NOT NULL,
+  `inventory_id` int NOT NULL,
+  `state` text NOT NULL,
   `created` datetime NOT NULL,
+  `task_id` int,
+  foreign key (`task_id`) references task(`id`) on delete set null,
   foreign key (`project_id`) references project(`id`) on delete cascade,
   foreign key (`inventory_id`) references project__inventory(`id`) on delete cascade
 );
