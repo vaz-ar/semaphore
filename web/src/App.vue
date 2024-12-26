953784b28e01089ce2990ba0c4bb2a20364e30b5
Denis Gukov <denguk@gmail.com>
2024-12-26 12:04:40 +0000

    fix(ui): drawer item title

--- web/src/App.vue
+++ web/src/App.vue
@@ -237,7 +237,7 @@
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{ $t('newProject') }}</v-list-item-title>
+            <v-list-item-title>{{ $t('newProject2') }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
